bb70c5e27df71db1206ac092571c8e24b26559fc
Roxanoel <roxane.noel92@gmail.com>
2022-07-04 10:42:24 +0100

    Removed testing lines from index.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
 import { createTodoItem } from "./todo.js";
 import { createAndAddProject, getProjects } from "./projects.js";
 
-console.table(getProjects());
-createAndAddProject('New Project');
-console.table(getProjects());
