de0fef53c813b4915ad84bcbf916b26cf4a9f5e0
Roxanoel <roxane.noel92@gmail.com>
2022-07-08 14:09:07 +0100

    Added factory function for card objects.

---
+++ src/cards.js
@@ -0,0 +1,26 @@
+import { createButtonWithId } from "./ui-utils";
+
+//#region FACTORY 
+function createCardContents(title, isDone, descr, priority, dueDate, isExpanded) {
+    return {
+        title: title,
+        isDone: isDone,
+        description: descr,
+        priorityLevel: priority,
+        dueDate: dueDate,
+        isExpanded: false
+    }
+}
+//#endregion
+
+//#region CARD CREATION 
+function createAndAddCard(title, isDone, descr, priority, dueDate, isExpanded) {
+    const contents = createCardContents(title, isDone, descr, priority, dueDate, isExpanded);
+
+    
+}
+
+function generateElementsForCard(contents) {
+    
+}
+//#endregion
